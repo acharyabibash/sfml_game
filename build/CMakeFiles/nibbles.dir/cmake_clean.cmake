d1db1ddce2453e211a42fc0aa7d3a7cbe2a587b2
acharyabibash <acharabibash@outlook.com>
2021-05-15 22:41:54 +0545

    First commit

---
+++ build/CMakeFiles/nibbles.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nibbles.dir/src/apple.cpp.obj"
+  "CMakeFiles/nibbles.dir/src/apple.cpp.obj.d"
+  "CMakeFiles/nibbles.dir/src/draw.cpp.obj"
+  "CMakeFiles/nibbles.dir/src/draw.cpp.obj.d"
+  "CMakeFiles/nibbles.dir/src/engine.cpp.obj"
+  "CMakeFiles/nibbles.dir/src/engine.cpp.obj.d"
+  "CMakeFiles/nibbles.dir/src/input.cpp.obj"
+  "CMakeFiles/nibbles.dir/src/input.cpp.obj.d"
+  "CMakeFiles/nibbles.dir/src/main.cpp.obj"
+  "CMakeFiles/nibbles.dir/src/main.cpp.obj.d"
+  "CMakeFiles/nibbles.dir/src/snakesection.cpp.obj"
+  "CMakeFiles/nibbles.dir/src/snakesection.cpp.obj.d"
+  "CMakeFiles/nibbles.dir/src/update.cpp.obj"
+  "CMakeFiles/nibbles.dir/src/update.cpp.obj.d"
+  "CMakeFiles/nibbles.dir/src/wall.cpp.obj"
+  "CMakeFiles/nibbles.dir/src/wall.cpp.obj.d"
+  "libnibbles.dll.a"
+  "nibbles.exe"
+  "nibbles.exe.manifest"
+  "nibbles.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nibbles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
